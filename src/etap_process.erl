9a155d196ba028887e4b57d74d1ccf415fe1a0b2
Nick Gerakines <nick@gerakines.net>
2008-11-28 13:49:09 -0800

    Adding MIT license to source files after getting the OK from Jeremy. Misc documentation changes.

--- src/etap_process.erl
+++ src/etap_process.erl
@@ -1,3 +1,26 @@
+%% Copyright (c) 2008 Nick Gerakines <nick@gerakines.net>
+%% 
+%% Permission is hereby granted, free of charge, to any person
+%% obtaining a copy of this software and associated documentation
+%% files (the "Software"), to deal in the Software without
+%% restriction, including without limitation the rights to use,
+%% copy, modify, merge, publish, distribute, sublicense, and/or sell
+%% copies of the Software, and to permit persons to whom the
+%% Software is furnished to do so, subject to the following
+%% conditions:
+%% 
+%% The above copyright notice and this permission notice shall be
+%% included in all copies or substantial portions of the Software.
+%% 
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+%% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+%% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+%% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+%% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+%% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+%% OTHER DEALINGS IN THE SOFTWARE.
+%%
 %% @doc Adds process/pid testing to the etap suite.
 -module(etap_process).
 
@@ -14,5 +37,6 @@ is_pid(Pid, Desc) ->
 is_alive(Pid, Desc) ->
     etap:ok(erlang:is_process_alive(Pid), Desc).
 
+%% @doc Assert that the current function of a pid is a given {M, F, A} tuple.
 is_mfa(Pid, MFA, Desc) ->
     etap:is({current_function, MFA}, erlang:process_info(Pid, current_function), Desc).
